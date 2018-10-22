47154b1c6a0b587c68f36019d514426f389a4240
LuizPampu <pampu.luiz@gmail.com>
2018-10-22 11:53:18 -0300

    Update js/requisicoes.js

--- js/requisicoes.js
+++ js/requisicoes.js
@@ -5,20 +5,22 @@ $(document).ready(function () {
         if ($('#nome').val() == '' || $('#email').val() == '' || $('#dtNascimento').val() == '') {
             alert("Todos campos devem ser preenchidos");
         } else {
-
-            var dados = $('#cadastrar-pessoa').serialize();
-
-            $.ajax({
-                method: 'POST',
-                dataType: 'json',
-                url: 'adciona-pessoa.php',
-                async: true,
-                data: dados
-            });
-
-            location.reload();
-            alert("Cadastrado");
+            CadastraPessoa();
         }
     });
 
 });
+
+function CadastraPessoa() {
+    var dados = $('#cadastrar-pessoa').serialize();
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: 'adciona-pessoa.php',
+        async: true,
+        data: dados
+    }).done(function () {
+        location.replace("http://localhost/teste-it4acio/listar-pessoas.php")
+    });
+}
